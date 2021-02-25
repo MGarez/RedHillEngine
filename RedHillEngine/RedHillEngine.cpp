411201d74d3913b2643e0534214848252010aa76
Mateo Garez <matgarez@gmail.com>
2021-02-25 18:10:54 +0100

    Cmake setup

---
+++ RedHillEngine/RedHillEngine.cpp
@@ -0,0 +1,12 @@
+﻿// RedHillEngine.cpp : Defines the entry point for the application.
+//
+
+#include "RedHillEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
